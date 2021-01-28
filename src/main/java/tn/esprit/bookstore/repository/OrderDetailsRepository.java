55eb84ada29304868dc077aca478575f4364d6f9
fatma mrabti <fatmamrabti8@gmail.com>
2021-01-28 22:37:18 +0100

    new update controller

--- src/main/java/tn/esprit/bookstore/repository/OrderDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.bookstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.bookstore.entities.OrderDetails;
-
-public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
-}
