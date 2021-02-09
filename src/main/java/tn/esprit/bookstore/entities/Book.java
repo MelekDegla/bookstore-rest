17876a7208b75082f716973f33d006fac3b9d3af
fatma-mrabti <fatmamrabti8@gmail.com>
2021-02-09 21:14:50 +0100

    recherche by pattern

--- src/main/java/tn/esprit/bookstore/entities/Book.java
+++ src/main/java/tn/esprit/bookstore/entities/Book.java
@@ -1,5 +1,8 @@
 package tn.esprit.bookstore.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -23,9 +26,11 @@ public class Book {
     private double price;
 
     @ManyToOne
+    @JsonIgnoreProperties({"eBooks", "pBooks"})
     private Author author;
 
     @ManyToOne
+    @JsonIgnoreProperties({"eBooks", "pBooks"})
     private Category category;
 
     @OneToMany(mappedBy = "book")
